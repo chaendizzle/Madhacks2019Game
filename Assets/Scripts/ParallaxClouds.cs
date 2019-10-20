b668a447866ffc71f504fb03c368636d683c1334
andrew3ng <andreweng1117@gmail.com>
2019-10-19 23:30:39 -0500

    Slight Update to animations; added clouds

---
+++ Assets/Scripts/ParallaxClouds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxClouds : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(-23.5f, 0.3f);
+    private readonly float SPEEDFACTOR = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += (Vector3) CameraMovement.CameraRect().size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
